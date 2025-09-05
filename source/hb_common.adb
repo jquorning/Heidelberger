289ba78bf2cbb67444ad9931b808f357edaf6e73
Jesper Quorning <jesper.quorning@gmail.com>
2025-09-05 08:20:04 +0200

    Full translation to edit

--- source/hb_common.adb
+++ source/hb_common.adb
@@ -6,29 +6,29 @@ is
    procedure Prepare_Items (Table : in out List_Table) is
    begin
       null;
-   end;
+   end Prepare_Items;
 
    function Current_Action (Item : List_Table) return String is ("XXX 1");
    function X_Get_List_Table (Item : String) return List_Table is
       List : List_Table;
    begin
-      return list;
-   end;
+      return List;
+   end X_Get_List_Table;
 
    function Typenow return String is ("XXX 2");
 
-   function Get_Post_Type_Object (Item : String) return Post_Record is
-      Rec : Post_Record;
+   function Get_Post_Type_Object (Item : String) return Post_Rec is
+      Rec : Post_Rec;
    begin
       return Rec;
-   end;
+   end Get_Post_Type_Object;
 
    procedure Post_Type_Object is null;
 
-   procedure Hb_Die (Why : String; Code : Integer := 0) is
+   procedure HB_Die (Why : String; Code : Integer := 0) is
    begin
       null;
-   end;
+   end HB_Die;
 
    procedure Parent_File is null;
    procedure Submenu_File is null;
@@ -36,11 +36,11 @@ is
 
    procedure Check_Admin_Referer (Item : String) is null;
 
-   function To_List (List : List_Type) return Assoc_List is ((1..0 => <>));
+   function To_List (List : List_Type) return Assoc_List is ((1 .. 0 => <>));
 
-   function To_Array (List : Assoc_List) return Array_Type is ((1..0 => <>));
+   function To_Array (List : Assoc_List) return Array_Type is ((1 .. 0 => <>));
 
-   function Hb_Get_Referer return String is ("XXX 3");
+   function HB_Get_Referer return String is ("XXX 3");
 
    function Remove_Query_Arg  (List : Array_Type; Item : String) return Unbounded_String
    is (Null_Unbounded_String);
@@ -48,44 +48,47 @@ is
    function Current_User_Can (Trait : Boolean) return Boolean is
    begin
       return True;
-   end;
+   end Current_User_Can;
 
    function Current_User_Can (Trait : String; Val : Assoc_Type) return Boolean is
    begin
       return True;
-   end;
+   end Current_User_Can;
 
-   function Admin_Url (Item : String) return Unbounded_String is (+"http://XXX 4");
+   function Admin_URL (Item : String) return Unbounded_String is (+"http://XXX 4");
 
    function Preg_Replace (Left : String; Right : Array_Type) return Integer is (1);
-   function X_REQUEST (Item : String) return Array_Type is (1..0 => <>);
+   function Preg_Replace (Left : String; Mid : String; Right : Array_Type) return Integer is (1);
+   function X_REQUEST (Item : String) return Array_Type is (1 .. 0 => <>);
    function Get_Post_Status_Object (N : Integer) return Boolean is (True);
 
-   function Get_Col (Db : Db_Type; Statement : Statement_type) return Array_Type is (1..0 => <>);
+   function Get_Col (Db : DB_Type; Statement : Statement_Type) return Array_Type
+      is (1 .. 0 => <>);
 
-   function Prepare (Db : Db_Type; Sql : String; Arg_1,Arg_2 : string) return Statement_Type
+   function Prepare (Db : DB_Type; Sql : String; Arg_1, Arg_2 : String) return Statement_Type
    is
-       S : Statement_type;
+      S : Statement_Type;
    begin
-       return S;
-   end;
+      return S;
+   end Prepare;
 
-   function Wpdb return Db_Type is
-      Db : Db_Type;
+   function Wpdb return DB_Type is
+      Db : DB_Type;
    begin
       return Db;
-   end;
+   end Wpdb;
 
    function Isset (Item : Array_Type) return Boolean is (True);
-   function Explode (Item : String; Table : Array_Type) return Array_Type is (1..0=><>);
+   function Explode (Item : String; Table : Array_Type) return Array_Type is (1 .. 0 => <>);
    function Implode (Item : String; Table : Array_Type) return String is ("XXX 5");
-   function Array_Map (Item : String; Table : Array_Type) return Array_Type is (1..0=><>);
+   function Implode (Item : String; Item_2 : Unbounded_String) return String is ("XXX-61");
+   function Array_Map (Item : String; Table : Array_Type) return Array_Type is (1 .. 0 => <>);
    function Empty (Table : Array_Type) return Boolean is (True);
 
    procedure HB_Redirect (Item : Unbounded_String) is null;
 
    function HB_Check_Post_Lock (Post_Id : Assoc_Type) return Boolean is (False);
-   function Hb_Trash_Post      (Post_Id : Assoc_Type) return Boolean is (False);
+   function HB_Trash_Post      (Post_Id : Assoc_Type) return Boolean is (False);
 
    function Add_Query_Arg (Item : String; N : Natural; Sb : Unbounded_String)
       return Unbounded_String is (Null_Unbounded_String);
@@ -99,42 +102,43 @@ is
       A : Assoc_Type;
    begin
       return A;
-   end;
+   end Build;
 
    function Absint (Item : Assoc_List) return String is ("1");
    procedure Bulk_Messages is null;
 
    function Array_Filter (List : Array_Type) return Assoc_List is
-       AL : constant Assoc_List := (1..0=><>);
+      AL : constant Assoc_List := (1 .. 0 => <>);
    begin
       return AL;
-   end;
-   function X_GET (Item : String) return Array_Type is (1..0=><>);
+   end Array_Filter;
+
+   function X_GET (Item : String) return Array_Type is (1 .. 0 => <>);
    function X_GET (Item : String) return String is ("XXX 6");
 
    procedure Add_Filter (Arg_1, Arg_2 : String; Arg_3, Arg_4 : Integer) is null;
    procedure Remove_Filter (Arg_1, Arg_2 : String; Arg_3 : Integer) is null;
 
    function HB_Untrash_Post (Item : Assoc_Type) return Boolean is (True);
-   function Hb_Delete_Attachment (Item : Assoc_Type) return Boolean is (True);
-   function Hb_Delete_Post (Item : Assoc_Type) return Boolean is (True);
+   function HB_Delete_Attachment (Item : Assoc_Type) return Boolean is (True);
+   function HB_Delete_Post (Item : Assoc_Type) return Boolean is (True);
 
-   function Get_Post (Id : Assoc_Type) return Post_Record is
-      Pr : Post_Record;
+   function Get_Post (Id : Assoc_Type) return Post_Rec is
+      Pr : Post_Rec;
    begin
       return Pr;
-   end;
+   end Get_Post;
 
    function Bulk_Edit_Posts (Item : Array_Type) return Array_Type is
-      A : constant Array_Type := (1..0=> <>);
+      A : constant Array_Type := (1 .. 0 => <>);
    begin
       return A;
-   end;
+   end Bulk_Edit_Posts;
 
    function Is_Array (Item : Assoc_List) return Boolean is (True);
 
    procedure Set (Arr : in out Array_Type; Key : String; Value : String) is null;
-   function Get (Arr : Array_Type; Key : string) return String is ("XXX 7");
+   function Get (Arr : Array_Type; Key : String) return String is ("XXX 7");
    function Count (Item : String) return String is ("XXX 8");
 
    Screen : aliased Screen_Type;
@@ -142,7 +146,7 @@ is
    function Get_Current_Screen return Screen_Access is
    begin
       return Screen'Access;
-   end;
+   end Get_Current_Screen;
 
    procedure Add_Help_Tab (Screen : in out Screen_Type; List : Array_Type) is null;
    procedure Set_Help_Sidebar (Screen : in out Screen_Type; Item : String) is null;
@@ -156,15 +160,64 @@ is
    function Apply_Filters (Item : String; S : Assoc_List; D : Assoc_List)
       return Assoc_List
    is
-      Al : constant Assoc_List := (1..0=> <>);
+      Al : constant Assoc_List := (1 .. 0 => <>);
    begin
       return Al;
-   end;
+   end Apply_Filters;
 
-   function Hb_Unslash (Item : String) return String is ("XXX 10");
-   function X_SERVER (Item : String) return String is ("XXX 11");
+   function HB_Unslash (Item : String) return String is ("XXX 10");
+--   function X_SERVER (Item : String) return String is ("XXX 11");
    procedure Add_Screen_Option (Item : String; List : Array_Type) is null;
 
-   function "abs" (List : Array_type) return String is ("XXX 12");
+   function "abs" (List : Array_Type) return String is ("XXX 12");
 
-end Hb_Common;
+   function Views (Item : List_Table) return String is ("XXX-31");
+   function Search_Box (Item : List_Table; L : String; R : String) return String is ("XXX-32");
+   function Display (Item : List_Table) return String is ("XXX-33");
+   function Inline_Edit (Item : List_Table) return String is ("XXX-34");
+   function Has_Items (Item : List_Table) return Boolean is (False);
+
+   function ESC_HTML (Item : String) return String is (Item);
+   function ESC_URL  (Item : String) return String is (Item);
+   function ESC_Attr (AL : Assoc_List) return String is ("XXX-41");
+   function ESC_Attrl (AL : Assoc_List) return String is ("XXX-42");
+
+   function Printf (Format : String; Arg_1 : String) return String is (Format & Arg_1);
+
+   function Get_Search_Query return String is ("XXX-43");
+
+   function Sprintf (Format : String; Arg_1 : String) return String is ("XXX-62");
+
+   function Number_Format_I18n (N : Integer) return String is ("XXX-63");
+   function HB_Nonce_URL (Url : String; Item : String) return String is ("XXX-64");
+   function Count (Al : Assoc_List) return Natural is (1);
+
+   function Get_Post_Type_Object (Item : Post_Rec) return String is ("XXX-65");
+   function Get_Post_Type_Object (Item : String) return String is ("XXX-66");
+
+   function Get_Post_Type (Item : Assoc_Type) return Post_Rec
+   is
+      Post : Post_Rec;
+   begin
+      return Post;
+   end Get_Post_Type;
+
+   function Get_Edit_Post_Link (Id : Assoc_Type) return String is ("XXX-67");
+
+   function Get (List : Array_Type; Arg_1 : String; Arg_2 : String) return Array_Type
+   is
+      A : constant Array_Type := (1 .. 0 => <>);
+   begin
+      return A;
+   end Get;
+
+   function Get (List : Array_Type; Arg_1 : String; Arg_2 : String) return String is ("XXX-68");
+
+   function As_Count_Message (List : Array_Type) return Count_Message_Type
+   is
+      C : Count_Message_Type;
+   begin
+      return C;
+   end As_Count_Message;
+
+end HB_Common;
--- source/hb_common.ads
+++ source/hb_common.ads
@@ -12,39 +12,48 @@ is
 
    type Cap_Type is
       record
-         Edit_Posts : Boolean := True;
+         Edit_Posts   : Boolean := True;
+         Create_Posts : Boolean := True;
       end record;
 
    type Lab_Type is
       record
-         Name : Unbounded_String;
+         Name              : Unbounded_String;
          Filter_Items_List : Unbounded_String;
          Items_List_Navigation : Unbounded_String;
-         Items_List : Unbounded_String;
+         Items_List   : Unbounded_String;
+         Add_New      : Unbounded_String;
+         Search_Items : Unbounded_String;
+         Edit_Items   : Unbounded_String;
+         Edit_Item    : Unbounded_String;
       end record;
 
-   type Post_Record is
+   type Post_Rec is
       record
-         Cap : Cap_Type;
+         Cap       : Cap_Type;
          Post_Type : Unbounded_String;
-         Labels : Lab_Type;
+         Labels    : Lab_Type;
       end record;
 
    type List_Table is tagged null record;
    function Get_Pagenum (Item : List_Table) return Natural;
    procedure Prepare_Items (Table : in out List_Table);
    function Current_Action (Item : List_Table) return String;
+   function Views (Item : List_Table) return String;
+   function Search_Box (Item : List_Table; L : String; R : String) return String;
+   function Display (Item : List_Table) return String;
+   function Inline_Edit (Item : List_Table) return String;
+   function Has_Items (Item : List_Table) return Boolean;
    function X_Get_List_Table (Item : String) return List_Table;
 
-
    function Typenow return String;
-   function Get_Post_Type_Object (Item : String) return Post_Record;
-   procedure Post_Type_Object;
-   procedure Hb_Die (Why : String; Code : Integer := 0);
 
-   procedure Parent_File;
-   procedure Submenu_File;
-   procedure Post_New_File;
+--   procedure Post_Type_Object;
+   procedure HB_Die (Why : String; Code : Integer := 0);
+
+--   procedure Parent_File;
+--   procedure Submenu_File;
+--   procedure Post_New_File;
 
    procedure Check_Admin_Referer (Item : String);
 
@@ -61,48 +70,49 @@ is
    type Array_Access is access all Array_Type;
 
    type List_Type is array (Positive range <>) of Unbounded_String;
-   Function To_List (List : List_Type) return Assoc_List;
+   function To_List (List : List_Type) return Assoc_List;
    function To_Array (List : Assoc_List) return Array_Type;
-   function Hb_Get_Referer return String;
+   function HB_Get_Referer return String;
    function Remove_Query_Arg  (List : Array_Type; Item : String) return Unbounded_String;
 
    function Current_User_Can (Trait : Boolean) return Boolean;
    function Current_User_Can (Trait : String; Val : Assoc_Type) return Boolean;
-   function Admin_Url (Item : String) return Unbounded_String;
+   function Admin_URL (Item : String) return Unbounded_String;
 
    function Preg_Replace (Left : String; Right : Array_Type) return Integer;
+   function Preg_Replace (Left : String; Mid : String; Right : Array_Type) return Integer;
    function X_REQUEST (Item : String) return Array_Type;
    function Get_Post_Status_Object (N : Integer) return Boolean;
 
-   Type Statement_Type is null record;
-   type Db_Type is tagged null record;
+   type Statement_Type is null record;
+   type DB_Type is tagged null record;
 
-   function Get_Col (Db : Db_Type; Statement : Statement_type) return Array_Type;
-   function Prepare (Db : Db_Type; Sql : String; Arg_1,Arg_2 : string) return Statement_Type;
-   function Wpdb return Db_Type;
+   function Get_Col (Db : DB_Type; Statement : Statement_Type) return Array_Type;
+   function Prepare (Db : DB_Type; Sql : String; Arg_1, Arg_2 : String) return Statement_Type;
+   function Wpdb return DB_Type;
 
    function Isset (Item : Array_Type) return Boolean;
    function Explode (Item : String; Table : Array_Type) return Array_Type;
    function Implode (Item : String; Table : Array_Type) return String;
+   function Implode (Item : String; Item_2 : Unbounded_String) return String;
    function Array_Map (Item : String; Table : Array_Type) return Array_Type;
    function Empty (Table : Array_Type) return Boolean;
 
    procedure HB_Redirect (Item : Unbounded_String);
 
    function HB_Check_Post_Lock (Post_Id : Assoc_Type) return Boolean;
-   function Hb_Trash_Post      (Post_Id : Assoc_Type) return Boolean;
+   function HB_Trash_Post      (Post_Id : Assoc_Type) return Boolean;
 
    function Add_Query_Arg (Item : String; N : Natural; Sb : Unbounded_String)
       return Unbounded_String;
    function Add_Query_Arg (List : Array_Type; Sb : Unbounded_String)
       return Unbounded_String;
 
-   procedure Hb_Enqueue_Script (Item : String);
-   procedure Hb_Enqueue_Style  (Item : String);
+   procedure HB_Enqueue_Script (Item : String);
+   procedure HB_Enqueue_Style  (Item : String);
 
    function Build (Key : String; Value : String) return Assoc_Type;
    function Absint (Item : Assoc_List) return String;
-   procedure Bulk_Messages;
 
    function Array_Filter (List : Array_Type) return Assoc_List;
    function X_GET (Item : String) return Array_Type;
@@ -112,15 +122,15 @@ is
    procedure Remove_Filter (Arg_1, Arg_2 : String; Arg_3 : Integer);
 
    function HB_Untrash_Post (Item : Assoc_Type) return Boolean;
-   function Hb_Delete_Attachment (Item : Assoc_Type) return Boolean;
-   function Hb_Delete_Post (Item : Assoc_Type) return Boolean;
+   function HB_Delete_Attachment (Item : Assoc_Type) return Boolean;
+   function HB_Delete_Post (Item : Assoc_Type) return Boolean;
 
-   function Get_Post (Id : Assoc_Type) return Post_Record;
+   function Get_Post (Id : Assoc_Type) return Post_Rec;
    function Bulk_Edit_Posts (Item : Array_Type) return Array_Type;
    function Is_Array (Item : Assoc_List) return Boolean;
 
    procedure Set (Arr : in out Array_Type; Key : String; Value : String);
-   function Get (Arr : Array_Type; Key : string) return String;
+   function Get (Arr : Array_Type; Key : String) return String;
    function Count (Item : String) return String;
 
    type Screen_Id is new Integer;
@@ -141,10 +151,42 @@ is
    function Apply_Filters (Item : String; S : Assoc_List; D : Assoc_List)
       return Assoc_List;
 
-   function Hb_Unslash (Item : String) return String;
-   function X_SERVER (Item : String) return String;
+   function HB_Unslash (Item : String) return String;
+   X_SERVER : Array_Type := (1 .. 0 => <>); -- (Item : String) return String;
    procedure Add_Screen_Option (Item : String; List : Array_Type);
 
-   function "abs" (List : Array_type) return String;
+   function "abs" (List : Array_Type) return String;
 
-end Hb_Common;
+   function ESC_HTML (Item : String) return String;
+   function ESC_URL  (Item : String) return String;
+   function ESC_Attr (AL : Assoc_List) return String;
+   function ESC_Attrl (AL : Assoc_List) return String;
+
+   function Printf (Format : String; Arg_1 : String) return String;
+
+   function Get_Search_Query return String;
+
+   function Sprintf (Format : String; Arg_1 : String) return String;
+   function Number_Format_I18n (N : Integer) return String;
+   function HB_Nonce_URL (Url : String; Item : String) return String;
+   function Count (Al : Assoc_List) return Natural;
+
+   function Get_Post_Type_Object (Item : Post_Rec) return String;
+   function Get_Post_Type_Object (Item : String) return Post_Rec;
+   function Get_Post_Type_Object (Item : String) return String;
+   function Get_Post_Type (Item : Assoc_Type) return Post_Rec;
+
+   function Get_Edit_Post_Link (Id : Assoc_Type) return String;
+
+   function Get (List : Array_Type; Arg_1 : String; Arg_2 : String) return Array_Type;
+   function Get (List : Array_Type; Arg_1 : String; Arg_2 : String) return String;
+
+   type Count_Message_Type is
+      record
+         Count   : Natural;
+         Message : Unbounded_String;
+      end record;
+
+   function As_Count_Message (List : Array_Type) return Count_Message_Type;
+
+end HB_Common;
