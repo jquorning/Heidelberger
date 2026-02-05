04234c75e2418eefb6a35d51b34af898b6124cb6
Jesper Quorning <jesper.quorning@gmail.com>
2026-02-05 14:16:44 +0100

    Silence apply_filters

--- source/heidelberger.adb
+++ source/heidelberger.adb
@@ -9,6 +9,7 @@ procedure Heidelberger is
 begin
    Logging.Silence ("inc_plugins.apply_filters");
    Logging.Silence ("do_action");
+   Logging.Silence ("apply_filters");
 
    HB_Server.Start;
    HB_Server.Wait;
